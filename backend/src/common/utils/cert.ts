e4ebf1f420928d6f12187ddf2b89229893d3e051
Happy-negi238 <happynegi238@gmail.com>
2026-07-14 02:40:15 +0530

    add dotenv support for loading private and public keys

--- backend/src/common/utils/cert.ts
+++ backend/src/common/utils/cert.ts
@@ -1,5 +1,9 @@
 import { readFileSync } from "node:fs";
 import path from "node:path";
 
-export const PRIVATE_KEY = readFileSync(path.resolve("cert/private.pem"), "utf-8");
-export const PUBLIC_KEY = readFileSync(path.resolve("cert/public.pem"), "utf-8");
+//import dotenv from "dotenv"; then get PRIVATE_KEY and PUBLIC_KEY from .env file
+import dotenv from "dotenv";
+dotenv.config();
+
+export const PRIVATE_KEY = process.env.PRIVATE_KEY?.replace(/\\n/g, "\n") || readFileSync(path.resolve("cert/private.pem"), "utf-8");
+export const PUBLIC_KEY = process.env.PUBLIC_KEY?.replace(/\\n/g, "\n") || readFileSync(path.resolve("cert/public.pem"), "utf-8");
